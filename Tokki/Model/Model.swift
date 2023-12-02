a4f897d4bd3309cc1595493bb83bdf56c2113b4b
Dariya Gecher <dariya.gecher@gmail.com>
2023-12-02 13:55:01 +0600

    contuning work via git

---
+++ Tokki/Model/Model.swift
@@ -0,0 +1,13 @@
+//
+//  Model.swift
+//  Tokki
+//
+//  Created by Dariya Gecher on 02.12.2023.
+//
+
+import SwiftUI
+import Combine
+
+class Model: ObservableObject {
+    @Published var showDetail: Bool = false
+}
