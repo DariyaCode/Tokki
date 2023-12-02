d5cd41697583af01710e393bd5599bd0e1e7d806
Dariya Gecher <dariya.gecher@gmail.com>
2023-12-02 11:55:01 +0600

    Initial Commit

---
+++ Tokki/TokkiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TokkiApp.swift
+//  Tokki
+//
+//  Created by Dariya Gecher on 02.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct TokkiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
